ab23631ae8ea9288d439eb4c452bb4f6dd98ebff
MegaPapka <penpineapplefruit@gmail.com>
2025-06-16 16:19:34 +0300

    added 2 new project

---
+++ Homework_5.1/Program3.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Enter a integer number 1: ");
+decimal firstNumber = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Enter a integer number 2: ");
+decimal secondNumber  = Convert.ToDecimal(Console.ReadLine());
+
+try
+{
+    if (secondNumber == 0)
+        throw new DivisionByZeroException();
+    else
+        Console.WriteLine(firstNumber / secondNumber);
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("You cannot divide by zero");
+}
+catch (Exception)
+{
+    Console.WriteLine("Something went wrong");
+}
+finally
+{
+    Console.WriteLine("Calculation ended");
+}
+
+public class DivisionByZeroException : Exception
+{
+    public DivisionByZeroException() : base()
+    {
+        
+    }
+}
---
+++ Homework_5.2/Program4.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter number: ");
+
+try
+{
+    string input = Console.ReadLine();
+    decimal number = Convert.ToDecimal(input);
+    
+    Console.WriteLine($"The number is: {number}");
+}
+catch (Exception)
+{
+    Console.WriteLine("Entered not number");
+}
+finally
+{
+    Console.WriteLine("Calculation ended");
+}
